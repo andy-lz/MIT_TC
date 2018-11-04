5af87f8938caf00a10bd643d250d143f3345ecdd
evaniahong <evaniahong@users.noreply.github.com>
2018-11-04 13:09:52 -0600

    connecting options trading logic

--- case_files/Barclays Options/options_bot.py
+++ case_files/Barclays Options/options_bot.py
@@ -1,16 +1,21 @@
 import tradersbot as tt
 import random
+import trade_logic
 import numpy as np
 
 t = tt.TradersBot(host='127.0.0.1', id='trader0', password='trader0')
 
 # Keeps track of prices
 SECURITIES = {}
+HIST_VOL_CURVE = {}
+TIME = 0
 
 # Initializes the prices
 def ack_register_method(msg, order):
 	global SECURITIES
+	#print msg
 	security_dict = msg['case_meta']['securities']
+	#print security_dict
 	for security in security_dict.keys():
 		if not(security_dict[security]['tradeable']):
 			continue
@@ -19,16 +24,41 @@ def ack_register_method(msg, order):
 # Updates latest price
 def market_update_method(msg, order):
 	global SECURITIES
-	print msg
-	time = msg['elapsed_time']
+	global TIME
+	TIME = msg['elapsed_time']
+	# TODO make this take into account best bids/offers instead
 	SECURITIES[msg['market_state']['ticker']] = msg['market_state']['last_price']
 
+
+
+
+def get_order(curr_pos):
+	global SECURITIES
+	global TIME
+	global HIST_VOL_CURVE
+	OPTION_DICT = trade_logic.get_opt_dict(SECURITIES, TIME)
+	CURR_VOL_CURVE = trade_logic.get_vol_curve(OPTION_DICT)
+	SMOOTHED_VOL_CURVE, HIST_VOL_CURVE = trade_logic.get_smoothed(CURR_VOL_CURVE, HIST_VOL_CURVE)
+	orders = trade_logic.execute_trade(CURR_VOL_CURVE, SMOOTHED_VOL_CURVE, curr_pos, OPTION_DICT)
+
+	return orders
+
+# Buys or sells in a random quantity every time it gets an update
 # Buys or sells in a random quantity every time it gets an update
 # You do not need to buy/sell here
 def trader_update_method(msg, order):
 	global SECURITIES
-	#print msg
+	print TIME
 	positions = msg['trader_state']['positions']
+	orders = get_order(positions)
+	for security in orders.keys():
+		quant = orders[security]
+		if quant > 0:
+			order.addBuy(security, quantity=quant, price=SECURITIES[security])
+		elif quant < 0:
+			order.addSell(security, quantity=-quant, price=SECURITIES[security])
+
+	'''
 	for security in positions.keys():
 		if random.random() < 0.5:
 			quant = 10*random.randint(1, 10)
@@ -36,6 +66,7 @@ def trader_update_method(msg, order):
 		else:
 			quant = 10*random.randint(1, 10)
 			#order.addSell(security, quantity=quant,price=SECURITIES[security])
+	'''
 
 ###############################################
 #### You can add more of these if you want ####
--- case_files/Barclays Options/trade_logic.py
+++ case_files/Barclays Options/trade_logic.py
@@ -1,10 +1,9 @@
 import json
-from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from py_vollib.ref_python.black_scholes import black_scholes, implied_volatility
+from py_vollib.black.greeks.analytical import delta, vega
 import re
-
 
 UNDERLYING_TICKER = 'TMXFUT'
 TRADING_THRESHOLD = 0.035
@@ -32,9 +31,9 @@ def get_opt_dict(sec_price_dict, T):
             opt_type = str(opt_type).lower()
             price = sec_price_dict[security]
 
-            print security
+            #print security
             try:
-                print (price, und_price, strike, ttm, 0, opt_type)
+                #print (price, und_price, strike, ttm, 0, opt_type)
                 iv = implied_volatility.implied_volatility(price, und_price, strike, ttm, 0, opt_type)
             except:
                 iv = 0
@@ -48,10 +47,9 @@ def get_opt_dict(sec_price_dict, T):
                             "dividend_rate": 0,
                             "implied_vol": iv }
 
-            print option_char
+            #print option_char
             OPTION_DICT[security] = option_char
     return OPTION_DICT
-
 
 def get_vol_curve(OPTION_DICT):
     CURR_VOL_CURVE = {}
@@ -62,7 +60,6 @@ def get_vol_curve(OPTION_DICT):
         put_iv = OPTION_DICT[put_opt]["implied_vol"]
         CURR_VOL_CURVE[strike] = (call_iv + put_iv)/2.0
     return CURR_VOL_CURVE
-
 
 def get_smoothed(CURR_VOL_CURVE, HIST_VOL_CURVE):
     result = {}
@@ -85,12 +82,24 @@ def get_smoothed(CURR_VOL_CURVE, HIST_VOL_CURVE):
     fitted_val = np.polyval(coef, x)
 
     SMOOTHED_VOL_CURVE = dict(zip(x, fitted_val))
-    return SMOOTHED_VOL_CURVE
+    return SMOOTHED_VOL_CURVE, HIST_VOL_CURVE
 
-
-def get_greeks(SECURITIES):
+def get_greeks(OPTION_DICT):
     delta_dict = {}
     vega_dict = {}
+    for security in OPTION_DICT.keys():
+        flag = OPTION_DICT[security]['opt_type']
+        F = OPTION_DICT[security]['und_price']
+        K = OPTION_DICT[security]['strike']
+        t = OPTION_DICT[security]['ttm']
+        r = 0
+        sigma = OPTION_DICT[security]['implied_vol']
+        d1 = delta(flag, F, K, t, r, sigma)
+        v1 = vega(flag, F, K, t, r, sigma)
+        delta_dict[security] = d1
+        vega_dict[security] = v1
+
+    '''
     delta = re.compile(r'T(\d*)([CP])_delta$')
     vega = re.compile(r'T(\d*)([CP])_vega$')
     for security in SECURITIES:
@@ -101,21 +110,22 @@ def get_greeks(SECURITIES):
             delta_dict[security] = SECURITIES[security]
         if bool(vega_groups):
             vega_dict[security] = SECURITIES[security]
+    '''
     return delta_dict, vega_dict
 
-def update_greeks(pos_dict, SECURITIES):
+def update_greeks(pos_dict, OPTION_DICT):
     # TODO make sure to take into account futures
-    delta_dict, vega_dict = get_greeks(SECURITIES)
+    delta_dict, vega_dict = get_greeks(OPTION_DICT)
     greeks_exposure_dict = {}
-    print pos_dict
-    print delta_dict
+    #print pos_dict
+    #print delta_dict
     for security in pos_dict.keys():
         if security == UNDERLYING_TICKER:
             greeks_exposure_dict[security] = {"delta": pos_dict[security] * 1,
                                               "vega": pos_dict[security] * 0}
         else:
-            greeks_exposure_dict[security] = {"delta": pos_dict[security] * delta_dict[security + "_delta"],
-                                              "vega": pos_dict[security] * vega_dict[security + "_vega"]}
+            greeks_exposure_dict[security] = {"delta": pos_dict[security] * delta_dict[security],
+                                              "vega": pos_dict[security] * vega_dict[security]}
 
     return greeks_exposure_dict
 
@@ -125,11 +135,11 @@ def hedge_delta(greeks_exposure_dict):
     target_und_pos = - total_delta
     return target_und_pos
 
-def check_target_trade(curr_pos, order_pos):
+def check_target_trade(curr_pos, order_pos, OPTION_DICT):
     target_dict = {k: curr_pos.get(k, 0) + order_pos.get(k, 0) for k in set(curr_pos) | set(order_pos)}
     if UNDERLYING_TICKER not in target_dict.keys():
         target_dict[UNDERLYING_TICKER] = 0
-    target_greeks = update_greeks(target_dict, SECURITIES)
+    target_greeks = update_greeks(target_dict, OPTION_DICT)
     total_delta = sum([target_greeks[x]['delta'] for x in target_greeks.keys()])
     total_vega = sum([target_greeks[x]['vega'] for x in target_greeks.keys()])
     if abs(total_vega) > 2800:
@@ -140,7 +150,7 @@ def check_target_trade(curr_pos, order_pos):
     return target_dict
 
 
-def execute_trade(VOL_CURVE, VOL_SPLINE, curr_pos):
+def execute_trade(VOL_CURVE, VOL_SPLINE, curr_pos, OPTION_DICT):
     orders = {}
     for strike in range(80, 120):
         perc_diff = (VOL_CURVE[strike] - VOL_SPLINE[strike])/VOL_CURVE[strike]
@@ -153,11 +163,10 @@ def execute_trade(VOL_CURVE, VOL_SPLINE, curr_pos):
             security = "T" + str(strike) + "C"
             qty = 1000.0 * perc_diff
             orders[security] = qty
-    adj_target_pos = check_target_trade(curr_pos, orders)
+    adj_target_pos = check_target_trade(curr_pos, orders, OPTION_DICT)
     submit_orders = {k: round(adj_target_pos.get(k, 0) - curr_pos.get(k, 0)) for k in set(adj_target_pos) | set(curr_pos)}
     return submit_orders
-
-
+'''
 
 T = 1
 with open('C:\Users\evani\OneDrive\Desktop\MIT_TC\case_files\Barclays Options\sample_0.json') as f:
@@ -173,36 +182,9 @@ curr_pos = {}
 
 OPTION_DICT = get_opt_dict(SECURITIES, T)
 CURR_VOL_CURVE = get_vol_curve(OPTION_DICT)
-SMOOTHED_VOL_CURVE = get_smoothed(CURR_VOL_CURVE, HIST_VOL_CURVE)
-orders = execute_trade(CURR_VOL_CURVE, SMOOTHED_VOL_CURVE, curr_pos)
+SMOOTHED_VOL_CURVE, HIST_VOL_CURVE = get_smoothed(CURR_VOL_CURVE, HIST_VOL_CURVE)
+orders = execute_trade(CURR_VOL_CURVE, SMOOTHED_VOL_CURVE, curr_pos, OPTION_DICT)
 print orders
 
 
-
-
-#print SECURITIES
-
-
 '''
-get_opt_dict(SECURITIES, T)
-get_vol_curve(OPTION_DICT, SECURITIES["TMXFUT"])
-VOL_SPLINE = get_smoothed(VOL_CURVE)
-
-
-
-execute_trade(VOL_CURVE, VOL_SPLINE)
-
-bleh = pd.DataFrame(OPTION_DICT)
-#bleh.to_csv("opt_dict0.csv")
-
-#print OPTION_DICT
-#print VOL_CURVE
-#plt.plot(VOL_CURVE.keys(), VOL_CURVE.values())
-
-#print VOL_CURVE
-#vol_df = pd.Series(VOL_CURVE)
-#vol_df.to_csv("vol_curve.csv")
-
-
-'''
-
